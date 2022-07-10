a9faf05366458a6c342694b518c488b327d10f43
giordanosilveira <giordanovraau0204@gmail.com>
2022-07-10 18:20:35 -0300

    Mod: P3

--- SO/ppos_core.c
+++ SO/ppos_core.c
@@ -6,12 +6,32 @@
 task_t *current_task;
 task_t *main_task;
 task_t *old_task;
-task_t *dispacher;
+task_t *dispatcher_task;
 task_t *ready_tasks;
 
 int user_tasks = 0;
 int current_id = 0;
 
+/**
+ * @brief Inicializa as estruturas internas da
+ * estutura task_t.
+ * 
+ * @param task (task_t*) : Uma tarefa para ser inicializada
+ */
+void init_struct_task(task_t *task){
+
+    task->id = current_id + 1;
+    task->next = task->prev = NULL;
+    task->id = READY;
+
+    current_id++;
+}
+
+
+/**
+ * @brief Inicializa as variáveis 
+ * do Sistema Operacional.
+ */
 void ppos_init () {
 
     #ifdef DEBUG
@@ -34,11 +54,63 @@ void ppos_init () {
         fprintf(stderr, "Erro ao criar o contexto para a função main\n");
         exit(1);
     }
+
+    dispatcher_task = malloc(sizeof(task_t));
+    if (! dispatcher_task){
+        fprintf(stderr, "O ponteiro do dispatcher não pode ser inicializado\n");
+        exit(1);
+    }
+    task_create(dispatcher_task,dispatcher, NULL);
     
-    main_task->id = current_id;
+    ready_tasks = NULL;
+
+    init_struct_task(dispatcher_task);
+    init_struct_task(main_task);
+    
     current_task = main_task;
 }
 
+task_t* escalonador () {
+
+
+
+}
+
+void dispatcher () {
+
+    task_t *next_task;
+    while (user_tasks) {
+        next_task = escalonador();
+
+        if (next_task) {
+
+            if (task_switch(next_task) < 0){
+                fprintf(stderr, "Erro ao troca para a proxima tarefa\n");
+                exit(1);
+            }
+
+            switch (next_task->status){
+                case FINISHED:
+                    if(! queue_remove((queue_t **)(ready_tasks), (queue_t*)(next_task)))
+                        exit(1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    task_exit(0);
+}
+
+/**
+ * @brief Cria uma tarefa.
+ * 
+ * @param task (taks_t*) : Ponteiro que será criada a tarefa.
+ * @param start_func (function *) : Função da tarefa.
+ * @param arg (void *) : Argumento da função passada.
+ * 
+ * @return int : -1, se der algum erro; id da tarefa, se der certo.
+ */
 int task_create(task_t *task, void (*start_func)(void *), void *arg) {
 
     #ifdef DEBUG
@@ -51,7 +123,7 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
     //Testa para ver se o contexto foi criado
     if (getcontext(&context) == -1){
         fprintf(stderr, "Erro ao criar a tarefa %d", current_id + 1);
-        exit(0);
+        return -1;
     }
 
     //Inicializa a pilha para o contexto atual
@@ -76,13 +148,19 @@ int task_create(task_t *task, void (*start_func)(void *), void *arg) {
 
     //O contexto da tarefa atual recebe o contexto específicado
     task->context = context;
-    task->id = current_id + 1;
+    init_struct_task(task);
 
-    //Incrementa o ID
-    current_id = current_id + 1;
+    queue_append((queue_t**)(&ready_tasks), (queue_t*)(task));
+    user_tasks++;
     return current_id;
 }
-
+/**
+ * @brief Troca a tarefa atual com a passada no argumento
+ *
+ * @param task (_task_t*_) | Tarefa a ser executada
+ *  
+ * @return int : -1 se der algum erro, 0 se a tarefa foi trocada
+ */
 int task_switch (task_t *task) {
 
     #ifdef DEBUG
@@ -105,15 +183,25 @@ int task_switch (task_t *task) {
 
     return 0;
 }
-
+/**
+ * @brief Termina uma tarefa
+ * 
+ * @param exit_code (_int_) : Código para terminar
+ *  
+ */
 void task_exit(int exit_code){
 
     #ifdef DEBUG
     printf("tast_exit: tarefa %d sendo encerrada\n", current_task->id);
     #endif
 
+    if (exit_code == 0) {
+        current_task->status = FINISHED;
+        user_tasks--;
+    }
+
     old_task = current_task;
-    task_switch(main_task);
+    task_switch(dispatcher_task);
 }
 
 /**
@@ -136,4 +224,7 @@ int task_id(){
 
 void task_yield() {
 
+    current_task->status = STOP;
+    dispatcher_task = READY;
+    task_switch(dispatcher_task);
 }
